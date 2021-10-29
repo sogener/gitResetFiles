28921792522615095f2ed1506142568c7d763214
Kalinin Gleb <glebstuck@yandex.ru>
2021-10-29 12:10:42 +0500

    added git branch deleting

--- app/main.php
+++ app/main.php
@@ -3,14 +3,34 @@
 $inputFile = fopen('../input', 'r+');
 $newFile = fopen('../result', 'r+');
 
-while (!feof($inputFile)) {
-    $lineOfText = fgets($inputFile);
-//    Чистка пробелов в начале и конце строки
-    $lineOfText = trim($lineOfText);
+$userInput = (int) readline("Выберите режим для очистки файлов: 1 - очистка git файлов. 2 - очистка веток git. ");
 
-    if(!empty($lineOfText)) {
-        $lineOfText = 'rm ' . $lineOfText;
-    }
-    fwrite($newFile, $lineOfText . PHP_EOL);
+switch ($userInput) {
+    case 1:
+        while (!feof($inputFile)) {
+            $lineOfText = fgets($inputFile);
+//    Чистка пробелов в начале и конце строки
+            $lineOfText = trim($lineOfText);
+
+            if(!empty($lineOfText)) {
+                $lineOfText = 'rm ' . $lineOfText;
+            }
+            fwrite($newFile, $lineOfText . PHP_EOL);
+        }
+        break;
+    case 2:
+        while (!feof($inputFile)) {
+            $lineOfText = fgets($inputFile);
+//    Чистка пробелов в начале и конце строки
+            $lineOfText = trim($lineOfText);
+
+            if(!empty($lineOfText)) {
+                $lineOfText = 'git branch -d ' . $lineOfText;
+            }
+            fwrite($newFile, $lineOfText . PHP_EOL);
+        }
 }
 return fclose($inputFile);
+
+
+
